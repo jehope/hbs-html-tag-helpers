75f28a767d9dfbdd2c6164a6349864f29373d774
jehope <jehope2@gmail.com>
2013-12-02 16:05:59 -0500

    Attempted to setup tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var module = require('../src/hbs-html-tag-helpers');
+var template = require('createTagBlock.hbs');
 var assert = chai.assert;
 
 describe('Your Module', function () {
