2c79d1e0182f30800e9c88cb321468f7ba92ab63
jehope <jehope2@gmail.com>
2014-01-07 09:31:25 -0500

    Scoped variables and commented

--- src/hbs-html-tag-helpers.js
+++ src/hbs-html-tag-helpers.js
@@ -1,17 +1,25 @@
 'use strict';
 
 var Handlebars = require("hbsfy/runtime");
+var tagReturn,link;
+
+/*********************
+ *  HTML Tag Helper  *
+ *********************/
 
 Handlebars.registerHelper('createTagBlock', function() {
+	// if tag is a link
 	if(this.tag === 'a') {
-		var link = (this.href) ? this.href : this.value;
-		var tagReturn = '<'+this.tag + ' href="'+link+'" >' + this.value + '</'+this.tag+ '>';
+		link = (this.href) ? this.href : this.value;
+		tagReturn = '<'+this.tag + ' href="'+link+'" >' + this.value + '</'+this.tag+ '>';
 	}
+	// if tag is an image
 	else if ( this.tag === 'img') {
-		var tagReturn = '<'+this.tag + ' src="'+this.value+'" />';
+		tagReturn = '<'+this.tag + ' src="'+this.value+'" />';
 	}
+	// all other tags TODO: some more tags need attention ie. iframe.
 	else {
-		var tagReturn = '<'+this.tag + '>' + this.value + '</'+this.tag+ '>';
+		tagReturn = '<'+this.tag + '>' + this.value + '</'+this.tag+ '>';
 	}	
   return new Handlebars.SafeString(tagReturn);
 });
