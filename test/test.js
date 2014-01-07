a0523941c2b1cd89f2873085399031376df02e08
jehope <jehope2@gmail.com>
2014-01-07 09:25:31 -0500

    Setup test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var module = require('../src/hbs-html-tag-helpers');
-var template = require('createTagBlock.hbs');
+var template = require('../test/createTagBlock.hbs');
 var assert = chai.assert;
 
 describe('Your Module', function () {
@@ -10,3 +10,16 @@ describe('Your Module', function () {
         assert.ok(true);
     });
 });
+
+describe('Template', function () {
+
+    it('createTagBlock', function () {
+        assert.ok(true);
+        var myDiv = document.createElement('div');
+        myDiv.innerHTML = 'Hey';
+        document.body.appendChild(myDiv);
+
+    });
+});
+
+
