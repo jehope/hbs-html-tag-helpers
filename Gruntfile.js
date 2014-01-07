61bbdfeb764ef839f9114b35e9b2cff93c313b87
jehope <jehope2@gmail.com>
2014-01-07 09:25:24 -0500

    Switch to hbsfy instead of handlebars

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,9 @@ module.exports = function (grunt) {
             }
         },
         browserify: {
+            options : {
+                transform : ['hbsfy']
+            },
             test: {
                 src  : ['test/test.js'],
                 dest : '.tmp/index.js',
--- src/hbs-html-tag-helpers.js
+++ src/hbs-html-tag-helpers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Handlebars = require("handlebars-runtime");
+var Handlebars = require("hbsfy/runtime");
 
 Handlebars.registerHelper('createTagBlock', function() {
 	if(this.tag === 'a') {
